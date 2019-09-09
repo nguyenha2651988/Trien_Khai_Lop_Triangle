9dc6900879b2df362c5ab8da4897d63d1c576ee2
anhhaday <nguyenha26051988@gmail.com>
2019-09-09 11:36:15 +0700

    bai tap

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        Triangle triangle1 = new Triangle(3,5,7);
+
+
+        System.out.println(triangle1.getArea());
+        System.out.println(triangle1.getPerimeter());
+
+    }
+}
